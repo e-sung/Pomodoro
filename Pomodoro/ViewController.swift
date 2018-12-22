94c739e0ecb4b3e2b828b5aac46780bba3c7f279
e-sung <dev.esung@gmail.com>
2018-12-22 20:43:24 +0900

    Initial Commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by 류성두 on 22/12/2018.
+//  Copyright © 2018 Sungdoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
