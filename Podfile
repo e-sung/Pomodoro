2b76a6a76b36f08ddf8ef87613077a62fd914fb8
e-sung <dev.esung@gmail.com>
2018-12-23 09:52:51 +0900

    feat: add DesignableView

--- Podfile
+++ Podfile
@@ -5,7 +5,6 @@ target 'Pomodoro' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'HGCircularSlider'
-
   # Pods for Pomodoro
 
   target 'PomodoroTests' do
--- Pomodoro/DesignableView.swift
+++ Pomodoro/DesignableView.swift
@@ -2,99 +2,105 @@
 //  DesignableView.swift
 //  Pomodoro
 //
-//  Created by 류성두 on 22/12/2018.
+//  Created by 류성두 on 23/12/2018.
 //  Copyright © 2018 Sungdoo. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
+@IBDesignable
 class DesignableView: UIView {
-    @IBInspectable public var borderColor: UIColor = UIColor.clear {
-        didSet {
-            layer.borderColor = borderColor.cgColor
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
         }
     }
     
-    @IBInspectable public var borderWidth: CGFloat = 0 {
-        didSet {
-            layer.borderWidth = borderWidth
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
         }
     }
     
-    @IBInspectable public var cornerRadius: CGFloat = 0 {
-        didSet {
-            layer.cornerRadius = cornerRadius
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
         }
     }
     
-    @IBInspectable public var shadowColor: UIColor = UIColor.clear {
-        didSet {
-            layer.shadowColor = shadowColor.cgColor
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
         }
     }
     
-    @IBInspectable public var shadowRadius: CGFloat = 0 {
-        didSet {
-            layer.shadowRadius = shadowRadius
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
         }
     }
-    
-    @IBInspectable public var shadowOpacity: CGFloat = 0 {
-        didSet {
-            layer.shadowOpacity = Float(shadowOpacity)
-        }
-    }
-    
-    @IBInspectable public var shadowOffsetY: CGFloat = 0 {
-        didSet {
-            layer.shadowOffset.height = shadowOffsetY
-        }
-    }
-    
 }
 
+@IBDesignable
 class DesignableButton: UIButton {
-    @IBInspectable public var borderColor: UIColor = UIColor.clear {
-        didSet {
-            layer.borderColor = borderColor.cgColor
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
         }
     }
     
-    @IBInspectable public var borderWidth: CGFloat = 0 {
-        didSet {
-            layer.borderWidth = borderWidth
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
         }
     }
     
-    @IBInspectable public var cornerRadius: CGFloat = 0 {
-        didSet {
-            layer.cornerRadius = cornerRadius
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
         }
     }
     
-    @IBInspectable public var shadowColor: UIColor = UIColor.clear {
-        didSet {
-            layer.shadowColor = shadowColor.cgColor
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
         }
     }
     
-    @IBInspectable public var shadowRadius: CGFloat = 0 {
-        didSet {
-            layer.shadowRadius = shadowRadius
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
         }
     }
-    
-    @IBInspectable public var shadowOpacity: CGFloat = 0 {
-        didSet {
-            layer.shadowOpacity = Float(shadowOpacity)
-        }
-    }
-    
-    @IBInspectable public var shadowOffsetY: CGFloat = 0 {
-        didSet {
-            layer.shadowOffset.height = shadowOffsetY
-        }
-    }
-    
 }
+
+
