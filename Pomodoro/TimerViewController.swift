e5b440447d0eecbb3df8dae0cb288efe8b302060
e-sung <dev.esung@gmail.com>
2018-12-23 15:37:07 +0900

    feat: apply Interval to TImerViewController

--- Pomodoro/TimerViewController.swift
+++ Pomodoro/TimerViewController.swift
@@ -6,18 +6,86 @@
 //  Copyright © 2018 Sungdoo. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import UserNotifications
 import HGCircularSlider
+
+
 
 public class TimerViewController: UIViewController {
     
     @IBOutlet var mainSlider: CircularSlider!
-
+    @IBOutlet var labelTime: UILabel!
+    
+    var interval: Interval!
+    
+    var timer = Timer()
+    var targetMinute: TimeInterval = 25
+    var targetSeconds:TimeInterval {
+        return targetMinute * 60
+    }
+    var elapsedSeconds:TimeInterval = 0
+    var canSendLocalNotification = false
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        interval = FocusInterval()
+        interval.delegate = self
+        setUpInitialView()
     }
-
-
+    
+    func setUpInitialView(){
+        mainSlider.endPointValue = 0
+        mainSlider.maximumValue = CGFloat(targetSeconds)
+        mainSlider.isUserInteractionEnabled = false
+        let currentFontSize = labelTime.font.pointSize
+        labelTime.font = UIFont.monospacedDigitSystemFont(ofSize: currentFontSize, weight: .medium)
+    }
+    
+    func move(_ slider: CircularSlider, to time: TimeInterval) {
+        let point = CGFloat(time)
+        slider.endPointValue = point
+        slider.layoutIfNeeded()
+        valueChanged(slider)
+    }
+    
+    @IBAction func valueChanged(_ sender: CircularSlider) {
+        guard let seconds = TimeInterval(exactly: sender.endPointValue) else { return }
+        let date = Date(timeIntervalSince1970: seconds)
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("mm:ss")
+        labelTime.text = dateFormatter.string(from: date)
+    }
+    
+    @IBAction func playOrPauseButtonClicked(_ sender: UIButton) {
+        if sender.title(for: .normal) == "▶" {
+            sender.setTitle("||", for: .normal)
+            interval.startTimer()
+        }
+        else {
+            interval.pauseTimer()
+            sender.setTitle("▶", for: .normal)
+        }
+    }
+}
+extension TimerViewController: IntervalDelegate {
+    func timeElapsed(_ seconds: TimeInterval) {
+        move(mainSlider, to: seconds)
+    }
 }
 
+
+extension TimerViewController: UNUserNotificationCenterDelegate {
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
+
+//
+
+}
