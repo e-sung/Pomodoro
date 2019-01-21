0360699d7b94c020d436fb40f804cbeb739b5f2a
e-sung <dev.esung@gmail.com>
2019-01-21 10:09:50 +0900

    feat: showOrHideDashboardLabels given verticalSizeClass

--- Pomodoro/MainTimerViewController.swift
+++ Pomodoro/MainTimerViewController.swift
@@ -22,6 +22,10 @@ public class MainTimerViewController: TimerViewController {
     @IBOutlet var labelIntervalCount: UILabel!
     @IBOutlet var rippleButton: RippleButton!
     @IBOutlet var imageViewEggWhite: UIImageView!
+    @IBOutlet var dashboardLabels: [UILabel]!
+    @IBOutlet var labelTimeFocusedTodayTitle: UILabel!
+    @IBOutlet var labelTimeFocusedTodayContent: UILabel!
+    @IBOutlet var labelDashboardTitle: UILabel!
 
     // MARK: Properties
 
@@ -63,6 +67,7 @@ public class MainTimerViewController: TimerViewController {
         coordinator.animate(alongsideTransition: { [weak self] _ in
             guard let strongSelf = self else { return }
             var alphaOfEggWhite: CGFloat = 0
+            self?.showOrHideDashboardLabels(given: newCollection.verticalSizeClass)
             if newCollection.verticalSizeClass == .compact {
                 self?.mainSlider.trackFillColor = .clear
             } else {
@@ -78,8 +83,18 @@ public class MainTimerViewController: TimerViewController {
     public override func refreshViews(with interval: Interval) {
         super.refreshViews(with: interval)
         refreshMainSlider(with: interval)
+        
+        showOrHideDashboardLabels(given: traitCollection.verticalSizeClass)
         labelIntervalCount.text = "\(currentCycleCount) / \(maxCycleCount)"
         setAccessibilityHintOfLabelIntervalCount()
+    }
+    
+    func showOrHideDashboardLabels(given verticalSizeClass: UIUserInterfaceSizeClass) {
+        if verticalSizeClass == .compact {
+            dashboardLabels.forEach({ $0.isHidden = (interval is FocusInterval) })
+        } else {
+            dashboardLabels.forEach({ $0.isHidden = true })
+        }
     }
 
     public override func timeElapsed(_ seconds: TimeInterval) {
