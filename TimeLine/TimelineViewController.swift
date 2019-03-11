5e6993ff30b264b70ba6b327662a84deb1f58ad3
e-sung <dev.esung@gmail.com>
2019-03-11 19:48:59 +0900

    타임라인을 테이블뷰 기반으로 다시 작성

--- TimeLine/TimelineViewController.swift
+++ TimeLine/TimelineViewController.swift
@@ -12,14 +12,39 @@ import PomodoroUIKit
 
 class TimelineViewController: UIViewController {
     @IBOutlet var titleTextView: UITextView!
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
         titleTextView.text = ""
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+
     }
+}
 
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .clear
+        cell.contentView.backgroundColor = .clear
+        return cell
+    }
+}
 
+extension TimelineViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView(frame: CGRect.zero)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "progressCell")
+        return cell
+    }
 }
 
