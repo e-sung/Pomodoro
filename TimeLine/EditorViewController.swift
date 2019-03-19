4d3f9188d0a8be0f37e244164a63b844e3a4775a
e-sung <dev.esung@gmail.com>
2019-03-19 20:20:57 +0900

    feat: 키보드 높이 변화 대응

--- TimeLine/EditorViewController.swift
+++ TimeLine/EditorViewController.swift
@@ -16,8 +16,8 @@ open class EditorViewController: UIViewController {
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var heightOfTitleView: NSLayoutConstraint!
     @IBOutlet var heightOfBodyTextView: NSLayoutConstraint!
-    
     @IBOutlet var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet var topConstraint: NSLayoutConstraint!
     var disposeBag = DisposeBag()
     
     open override func loadView() {
@@ -32,29 +32,47 @@ open class EditorViewController: UIViewController {
         bodyTextView.delegate = self
         bodyTextView.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: bodyTextView.font!)
         adjustHeight(of: titleTextView, with: heightOfTitleView)
-        
         RxKeyboard.instance.visibleHeight.asObservable().bind(onNext: { [weak self] height in
             self?.bottomConstraint.constant = height
+            if height == 0 {
+                self?.topConstraint.constant = 0
+                UIView.animate(withDuration: 0.5, animations: {
+                    self?.view.layoutIfNeeded()
+                })
+            }
         })
         .disposed(by: disposeBag)
     }
-
 }
 
 extension EditorViewController: UITextViewDelegate {
     open func textViewDidChange(_ textView: UITextView) {
         if textView === titleTextView {
+            if textView.text.last == "\n" {
+                bodyTextView.becomeFirstResponder()
+                textView.text = textView.text.replace(from: "\n", to: "")
+            }
             adjustHeight(of: textView, with: heightOfTitleView, duration: 0.5)
         }
+        else if textView === bodyTextView {
+            topConstraint.constant = -1 * (titleTextView.frame.height + 14 + UIApplication.shared.statusBarFrame.height)
+            UIView.animate(withDuration: 0.5, animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            })
+        }
     }
-    
-    
+
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0 {
+            view.endEditing(true)
+        }
+    }
+
     func adjustHeight(of textView: UITextView, with height: NSLayoutConstraint, duration: TimeInterval = 0) {
         let fixedWidth = textView.frame.size.width
         let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
         height.constant = newSize.height
         UIView.animate(withDuration: duration, animations: { [weak self] in
-            
             self?.view.layoutIfNeeded()
         })
     }
