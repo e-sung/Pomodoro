186b19fc0a23e4f322f91a73f5877fcb0bc0050c
e-sung <dev.esung@gmail.com>
2019-10-16 15:41:19 +0900

    feat: 내게 할당 된 이슈들 가져오는 API 추가

---
+++ JiraSupport/FetchIssues.swift
@@ -0,0 +1,25 @@
+//
+//  FetchIssues.swift
+//  JiraSupport
+//
+//  Created by 류성두 on 2019/10/16.
+//  Copyright © 2019 Sungdoo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+func fetchIssues(then completionHandler: @escaping (Result<[String], Error>) -> Void) {
+    let url = "https://jira.flit.to:18443/rest/api/2/search?jql=assignee%20=%20currentUser()%20AND%20%20status%20=%20%22In%20Progress%22"
+    AF.request(url).responseJSON(completionHandler: { res in
+        let dict = res.value as? [String: Any]
+        let issues = dict?["issues"] as? [[String: Any]]
+        let issueKeys = issues?.compactMap({ $0["key"] as? String})
+        if let error = res.error {
+            completionHandler(.failure(error))
+        }
+        else {
+            completionHandler(.success(issueKeys ?? []))
+        }
+    })
+}
