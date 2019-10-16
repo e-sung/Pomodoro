6028c64074b1b16fd398f9eeaae4c9607c69c669
e-sung <dev.esung@gmail.com>
2019-10-16 16:06:01 +0900

    feat: 로그인/아웃 ViewController 추가

---
+++ JiraSupport/JiraLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JiraLoginViewController.swift
+//  JiraSupport
+//
+//  Created by 류성두 on 2019/10/16.
+//  Copyright © 2019 Sungdoo. All rights reserved.
+//
+
+import UIKit
+
+class JiraLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
