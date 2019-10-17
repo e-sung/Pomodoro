4bc91737b60f7928e4c9b64a3febd2f2582d1f95
e-sung <dev.esung@gmail.com>
2019-10-17 11:47:59 +0900

    feat : Support Catalyst

--- JiraSupport/MyIssuesViewController.swift
+++ JiraSupport/MyIssuesViewController.swift
@@ -12,9 +12,10 @@ public protocol MyIssueViewControllerDelegate: AnyObject {
     func didSelect(issue: Issue?)
 }
 
-public class MyIssuesViewController: UITableViewController {
+public class MyIssuesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     public weak var delegate: MyIssueViewControllerDelegate?
     var myIssues: [Issue] = []
+    @IBOutlet var tableView: UITableView!
 
     public static var storyboardInstance: MyIssuesViewController {
         let sb = UIStoryboard(name: "MyIssues", bundle: Bundle(for: MyIssuesViewController.self))
@@ -31,21 +32,25 @@ public class MyIssuesViewController: UITableViewController {
         })
     }
 
-    public override func numberOfSections(in _: UITableView) -> Int {
+    @IBAction func buttonCloseClicked(_: Any?) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    public func numberOfSections(in _: UITableView) -> Int {
         return 1
     }
 
-    public override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+    public func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return myIssues.count
     }
 
-    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = myIssues[indexPath.row].sumamry
         return cell
     }
 
-    public override func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
+    public func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
         let issue = myIssues[indexPath.row]
         delegate?.didSelect(issue: issue)
         dismiss(animated: true, completion: nil)
--- Podfile
+++ Podfile
@@ -5,8 +5,6 @@ target 'Pomodoro' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'HGCircularSlider'
-  pod 'RxSwift'
-  pod 'RxCocoa'
   # Pods for Pomodoro
 
   target 'PomodoroTests' do
