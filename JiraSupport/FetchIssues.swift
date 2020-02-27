353dd69d141c2640127558230dce6c07dd14cccf
Sungdoo Yoo <dev.esung@gmail.com>
2020-02-28 08:24:18 +0900

    feat: update jql

--- JiraSupport/FetchIssues.swift
+++ JiraSupport/FetchIssues.swift
@@ -19,7 +19,7 @@ var credentialHeader: HTTPHeader? {
 
 func fetchIssues(then completionHandler: @escaping (Result<[Issue], Error>) -> Void) {
     let jql = """
-    assignee = currentUser() AND status in (진행중, "개발 중")
+    assignee = currentUser() AND status in ("진행중", "개발 중", "In Progress")
     """.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
     guard let url = URL(string: "/rest/api/2/search?jql=\(jql)", relativeTo: mainJiraDomain) else {
         fatalError("Wrong Path for Issues")
